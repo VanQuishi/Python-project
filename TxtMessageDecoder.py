404e24f375775b16b2036211b50ec956c247f289
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2019-07-27 15:32:53 -0500

    Text Message Abbreviation Decoder
    User can input an abbreviation and the program will search inside its pre-existing dictionary for the correspoding definition.

---
+++ TxtMessageDecoder.py
@@ -0,0 +1,27 @@
+abbrDict = {
+    "AKA":"also known as",
+    "LOL":"laughing out loud",
+    "BFF":"best friends forever",
+    "IMHO":"in my humble opinion",
+    "TMI":"too much information",
+    "IDC":"I don't care",
+    "IDK":"I don't know",
+    "ILY":"I love you",
+    "OMG":"oh my God",
+}
+
+
+def search(word):
+
+    keyList = list(abbrDict.keys())
+
+    for item in keyList:
+        if word == item:
+            print(word, "means", abbrDict[item])
+
+
+
+key = input("Input an abbreviation: ")
+search(key)
+
+
